d180bcd57f204cfdd48f0909f1efc06bdfac9e3d
Ferit Cubukcuoglu <fcbk67@gmail.com>
2019-07-11 14:13:00 +0200

    liquid image transition added

---
+++ Liquid-image-transition/app.js
@@ -0,0 +1,14 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    // intensity: 0.3,
+    intensity1: 0.1,
+    intensity2: 0.2,
+    image1: "./ny.jpg",
+    image2: "./ny.jpg",
+    // angle: Math.PI/3,
+    
+
+    // image1: "./image1.png",
+    // image2: "./image2.png",
+    displacementImage: "./strip.png"
+})
